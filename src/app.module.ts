fb6e778856d534fb33687e9228c78d99ea3a8220
faiqahsan786 <faiq.git@gmail.com>
2023-09-01 19:42:25 +0500

    added gql packages and app module config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,22 @@ import { ConfigModule } from '@nestjs/config';
 import { typeOrmAsyncConfig } from './orm.config';
 import { HealthCheckController } from './healthCheck/HealthCheckController';
 import moduleImports from './autogen';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import GraphQLJSON from 'graphql-type-json';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      resolvers: { JSON: GraphQLJSON },
+      sortSchema: true,
+      context: ({ req }) => ({ req }),
+    }),
     ...moduleImports,
   ],
   controllers: [HealthCheckController],
